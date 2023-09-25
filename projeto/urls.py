a3ee970033bc28f545ab47026949a5d1b908ae43
Eduardo Alves Lins <eal3@cesar.school>
2023-09-25 16:53:10 -0300

    Modificando a response

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def myview (request):
+    return HttpResponse("Dudu")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',myview),
 ]
